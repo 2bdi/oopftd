1433156fca9409fa2d72fd9943028fa390308d29
2bdi <85628769+2bdi@users.noreply.github.com>
2021-09-21 17:18:26 +0700

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -12,39 +12,33 @@ namespace ООПФТД
 
         public void print()
         {
-            Console.Write("Экстремумы для функции f(x)=(x+1)^3+5*x^2");
-            Console.Write($"[{this.a};{this.b}]");
-            Console.Write($" при x: {this.result:0.000}");
-            Console.Write($"max: {this.max:0.000}");
-            Console.Write($"min: {this.min:0.000}");
+            Console.Write($"[{this.a};{this.b}]\n");
+            Console.Write($" при x: {this.result:0.000}\n");
+            Console.Write($"max: {this.max:0.000}\n");
+            Console.Write($"min: {this.min:0.000}\n");
         }
 
         public Gold(float a, float b)
         {
-            float e = 0.001f;
-            float x1, x2, y1, y2;
+            float x1 = 0, x2 = 0, y1 = 0, y2 = 0;
             this.a = a;
             this.b = b;
-            while(!(Math.Abs(b - a) < e))
+            while(!(Math.Abs(b - a) < 0.001f))
             {
                 x1 = b - ((b - a) / 1.618f);
                 x2 = a + ((b - a) / 1.618f);
                 y1 = MathF.Pow(x1 + 1, 3) + (5 * x1 * x1);
-                y2 = MathF.Pow(x2 + 1, 3) + (5 * x2 * x2);      
+                y2 = MathF.Pow(x2 + 1, 3) + (5 * x2 * x2);  
+                if (y1 >= y2)
+                {
+                    a = x1;
+                    this.min = (a + this.b) / 2;
+                } else {
+                    b = x2;
+                    this.max = (this.a + b) / 2;
+                }
             }
-            if (y1 >= y2)
-                a = x1;
-            else
-                b = x2;
-            this.min = (a + b) / 2;
-            a = this.a;
-            b = this.b;
-            if (y1 <= y2)
-                a = x1;
-            else
-                b = x2;
-            this.max = (a + b) / 2;
-            this.result = MathF.Pow(x1 + 1, 3) + (5 * MathF.Pow(x1, 2));
+            this.result = MathF.Pow(this.max + 1, 3) + (5 * MathF.Pow(this.min, 2));
         }
     }
 
@@ -55,6 +49,7 @@ namespace ООПФТД
             Gold x1 = new Gold(-2f, 2f);
             Gold x2 = new Gold(-8f, 0f);
 
+            Console.Write("Экстремумы для функции f(x)=(x+1)^3+5*x^2\n");
             x1.print();
             x2.print();
         }
